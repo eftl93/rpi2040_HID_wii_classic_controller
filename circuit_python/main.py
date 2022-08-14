b0e320b04a1ce6adb5ddb619b52e50f10d3547b1
efl93 <eftl93@gmail.com>
2022-08-14 10:08:34 -0500

    renamed file to main.py and initialized wii classic controller pro
    Initialized Wii controller pro without encryption.
    Extracted all button data.
    Created and HID device.
    Started sending data through usb depending on the button pressed.

--- circuit_python/main.py
+++ circuit_python/main.py
@@ -6,18 +6,18 @@ from digitalio import DigitalInOut, Direction, Pull
 import busio
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
 led = DigitalInOut(board.LED)
 led.direction = Direction.OUTPUT
 
 #setup a keyboard as a device
-keyboard = Keyboard(usb_hid.devices)
-
-
+kbd = Keyboard(usb_hid.devices)
+keyboard = KeyboardLayoutUS(kbd)
 
 #define that i2c will be used and the pins to be used are gp7 and gp6 at a frequency of 400KHz
 i2c=busio.I2C(board.GP7, board.GP6, frequency=400000)
+
 
 #Lock the I2C device before we try to scan
 while not i2c.try_lock():
@@ -38,12 +38,14 @@ try:
 finally:
     #unlock the i2c bus when finished scanning
     i2c.unlock()
-
+#setup variables before infinite loop
+#default value for keyboard_on is 0
+keyboard_on = 0;
 while True:
     led.value = True
-    time.sleep(.2)
+    time.sleep(.1)
     led.value = False
-    time.sleep(.2)
+    time.sleep(.1)
     while not i2c.try_lock():
         pass
     try:
@@ -51,12 +53,6 @@ while True:
         i2c.writeto(0x52,bytes([0x00]))
         wii_controller_frame = bytearray(6)
         i2c.readfrom_into(0x52,wii_controller_frame)
-        print("Byte 0:",bin(wii_controller_frame[0]))
-        print("Byte 1:",bin(wii_controller_frame[1]))
-        print("Byte 2:",bin(wii_controller_frame[2]))
-        print("Byte 3:",bin(wii_controller_frame[3]))
-        print("Byte 4:",bin(wii_controller_frame[4]))
-        print("Byte 5:",bin(wii_controller_frame[5]))
         #Extract Right X-Axis bits
         rx = (wii_controller_frame[2] & 0x80) >> 7
         rx = rx | ((wii_controller_frame[1] & 0xC0) >> 5)
@@ -67,10 +63,10 @@ while True:
         print("Right Joystick, Y-axis:", bin(ry))
         #Extract Left X-Axis bits and normalize to 5 bits like right stick
         lx = ((wii_controller_frame[0] & 0x3F) >> 1)
-        print("Left Joystick, X-axis:", bin(lx))
+        print("Left Joystick, X-axis: ", bin(lx))
         #Extract Left Y-Axis bits and normalize to 5 bits like right stick
         ly = ((wii_controller_frame[1] & 0x3F) >> 1)
-        print("Left Joystick, Y-axis:", bin(ly))
+        print("Left Joystick, Y-axis: ", bin(ly))
         #Extract digital directional pad
         du = (wii_controller_frame[5] & 0x01)
         dd = ((wii_controller_frame[4] & 0x40) >> 6)
@@ -82,19 +78,30 @@ while True:
         db = ((wii_controller_frame[5] & 0x40) >> 6)
         dx = ((wii_controller_frame[5] & 0x08) >> 3)
         dy = ((wii_controller_frame[5] & 0x20) >> 5)
-        print("a:",bin(da),"b:",bin(db),"x:",bin(dx),"y:",bin(dy))
+        print("a: ",bin(da),"   b:",bin(db),"    x:",bin(dx),"   y:",bin(dy))
         #Extract digital menu buttons
         minus = ((wii_controller_frame[4] & 0x10) >> 4)
         home = ((wii_controller_frame[4] & 0x08) >> 3)
         plus = ((wii_controller_frame[4] & 0x04) >> 2)
-        print("-:",bin(minus),"home:",bin(home),"+:",bin(plus))
+        print(" -:",bin(minus),"home:",bin(home),"    +:",bin(plus))
         #Extract Shoulder buttons
         zl = ((wii_controller_frame[5] & 0x80) >> 7)
         zr = ((wii_controller_frame[5] & 0x04) >> 2)
         lt = ((wii_controller_frame[4] & 0x20) >> 5)
         rt = ((wii_controller_frame[4] & 0x02) >> 1)
-        print("zl:",bin(zl),"zr:",bin(zr),"lt:",bin(lt),"rt:",bin(rt))
+        print("zl:",bin(zl),"  zr:",bin(zr),"   lt:",bin(lt),"  rt:",bin(rt))
+        print("------------------------------------------------")
+        print("------------------------------------------------")
     finally:
         #unlock the i2c bus when finished scanning
         i2c.unlock()
+    #use home button to turn on or off keyboard function. 
+    keyboard_on = bool(keyboard_on) ^ bool(not home)
+    print("State of keyboard_on flag: ", bool(keyboard_on))
+    
+    if keyboard_on:
+        keyboard.write(str(lx))
+        keyboard.write('\n')
+        keyboard.write(str(ly))
+        keyboard.write('\n')16
     
