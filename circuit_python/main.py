01bd46ce043a2dc8e436f75216e328d9baae02bc
efl93 <eftl93@gmail.com>
2022-08-15 21:44:39 -0500

    fix timings, packaged data in binary

--- circuit_python/main.py
+++ circuit_python/main.py
@@ -43,9 +43,9 @@ finally:
 keyboard_on = 0;
 while True:
     led.value = True
-    time.sleep(.1)
+    time.sleep(.04)
     led.value = False
-    time.sleep(.1)
+    time.sleep(.05)
     while not i2c.try_lock():
         pass
     try:
@@ -57,41 +57,41 @@ while True:
         rx = (wii_controller_frame[2] & 0x80) >> 7
         rx = rx | ((wii_controller_frame[1] & 0xC0) >> 5)
         rx = rx | ((wii_controller_frame[0] & 0xC0) >> 3)
-        print("Right Joystick, X-axis:", bin(rx))
+        #print("Right Joystick, X-axis:", bin(rx))
         #Extract Right Y-Axis bits
         ry = (wii_controller_frame[2] & 0x1F)
-        print("Right Joystick, Y-axis:", bin(ry))
+        #print("Right Joystick, Y-axis:", bin(ry))
         #Extract Left X-Axis bits and normalize to 5 bits like right stick
         lx = ((wii_controller_frame[0] & 0x3F) >> 1)
-        print("Left Joystick, X-axis: ", bin(lx))
+        #print("Left Joystick, X-axis: ", bin(lx))
         #Extract Left Y-Axis bits and normalize to 5 bits like right stick
         ly = ((wii_controller_frame[1] & 0x3F) >> 1)
-        print("Left Joystick, Y-axis: ", bin(ly))
+        #print("Left Joystick, Y-axis: ", bin(ly))
         #Extract digital directional pad
         du = (wii_controller_frame[5] & 0x01)
         dd = ((wii_controller_frame[4] & 0x40) >> 6)
         dr = ((wii_controller_frame[4] & 0x80) >> 7)
         dl = ((wii_controller_frame[5] & 0x02) >> 1)
-        print("up:",bin(du),"down:",bin(dd),"right:",bin(dr),"left:",bin(dl))
+        #print("up:",bin(du),"down:",bin(dd),"right:",bin(dr),"left:",bin(dl))
         #Extract digital action buttons
         da = ((wii_controller_frame[5] & 0x10) >> 4)
         db = ((wii_controller_frame[5] & 0x40) >> 6)
         dx = ((wii_controller_frame[5] & 0x08) >> 3)
         dy = ((wii_controller_frame[5] & 0x20) >> 5)
-        print("a: ",bin(da),"   b:",bin(db),"    x:",bin(dx),"   y:",bin(dy))
+        #print("a: ",bin(da),"   b:",bin(db),"    x:",bin(dx),"   y:",bin(dy))
         #Extract digital menu buttons
         minus = ((wii_controller_frame[4] & 0x10) >> 4)
         home = ((wii_controller_frame[4] & 0x08) >> 3)
         plus = ((wii_controller_frame[4] & 0x04) >> 2)
-        print(" -:",bin(minus),"home:",bin(home),"    +:",bin(plus))
+        #print(" -:",bin(minus),"home:",bin(home),"    +:",bin(plus))
         #Extract Shoulder buttons
         zl = ((wii_controller_frame[5] & 0x80) >> 7)
         zr = ((wii_controller_frame[5] & 0x04) >> 2)
         lt = ((wii_controller_frame[4] & 0x20) >> 5)
         rt = ((wii_controller_frame[4] & 0x02) >> 1)
-        print("zl:",bin(zl),"  zr:",bin(zr),"   lt:",bin(lt),"  rt:",bin(rt))
-        print("------------------------------------------------")
-        print("------------------------------------------------")
+        #print("zl:",bin(zl),"  zr:",bin(zr),"   lt:",bin(lt),"  rt:",bin(rt))
+        #print("------------------------------------------------")
+        #print("------------------------------------------------")
     finally:
         #unlock the i2c bus when finished scanning
         i2c.unlock()
@@ -103,17 +103,44 @@ while True:
     shoulder_plus_minus = (shoulder_plus_minus << 1) | rt
     shoulder_plus_minus = (shoulder_plus_minus << 1) | minus
     shoulder_plus_minus = (shoulder_plus_minus << 1) | plus
-
+    #formated value to active high
+    shoulder_plus_minus = (~shoulder_plus_minus)+(1<<6)
+    
+    #concatenation of dpad
+    dpad_buttons = 0
+    dpad_buttons = (dpad_buttons << 0) | du
+    dpad_buttons = (dpad_buttons << 1) | dd
+    dpad_buttons = (dpad_buttons << 1) | dl
+    dpad_buttons = (dpad_buttons << 1) | dr
+    #formated value to active high
+    dpad_buttons = (~dpad_buttons) + (1<<4)
+    
+    #concatenation of action buttons
+    daction_buttons = 0
+    daction_buttons = (daction_buttons << 1) | da
+    daction_buttons = (daction_buttons << 1) | db
+    daction_buttons = (daction_buttons << 1) | dx
+    daction_buttons = (daction_buttons << 1) | dy
+    #formated value to active high
+    daction_buttons = (~daction_buttons) + (1<<4)
+    
     #use home button to turn on or off keyboard function. 
     keyboard_on = bool(keyboard_on) ^ bool(not home)
-    print("State of keyboard_on flag: ", bool(keyboard_on))
+    if (not home):
+        time.sleep(.5)
+    #print("State of keyboard_on flag: ", bool(keyboard_on))
+    #print("d-pad: ", bin(dpad_buttons))
+    #print("action buttons: ", bin(daction_buttons))
+    #print("shoulder, + and -: ", bin(shoulder_plus_minus))
     
     if keyboard_on:
-        keyboard.write('start')
-        keyboard.write(str(lx))
-        keyboard.write(str(ly))
-        keyboard.write(str(rx))
-        keyboard.write(str(ry))
-        keyboard.write(str(shoulder_plus_minus))
+        keyboard.write('z')
+        keyboard.write(chr(lx + 33))
+        keyboard.write(chr(ly + 33))
+        keyboard.write(chr(rx + 33))
+        keyboard.write(chr(ry + 33))
+        #keyboard.write(chr(shoulder_plus_minus + 33))
+        keyboard.write(chr(dpad_buttons + 33))
+        keyboard.write(chr(daction_buttons + 33))
         keyboard.write('\n')
     
